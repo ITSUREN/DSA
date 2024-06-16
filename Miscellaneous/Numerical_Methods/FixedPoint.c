ef70d92f9aa7f040b2340b8bba7e3ab85b2892d7
Itsuren Kanakueta <itsurenkanakueta@gmail.com>
2024-06-16 18:40:36 +0545

    added Fixed Point Method

---
+++ Miscellaneous/Numerical_Methods/FixedPoint.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+
+#define MAX 20
+#define E 0.0001
+#define g(x) (exp(x)-2) //exp(x)-x-2=0, x=exp(x)-2
+
+int main()
+{   int count=1;
+    float x0,x1,error;
+    printf("Enter the initial guess valueP: ");
+    scanf("%f",&x0);
+    while (count<MAX) {
+        x1=g(x0); error=x1-x0;
+        if(fabs(error)<E) {
+            printf("The root is %f",x1);
+            printf("\nNo. of iterations is %d",count);
+            break;
+        } else {
+            x0=x1;
+            count++;
+            if(count>=MAX) {
+                printf("The process didn\'t converge.");
+                break;
+            }
+        }
+    }
+    return 0;
+}
