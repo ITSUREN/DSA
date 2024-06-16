d70f6904e631405052813e80605ace96fe5a17b7
Itsuren Kanakueta <itsurenkanakueta@gmail.com>
2024-06-16 18:41:11 +0545

    Added Lagrange's Method

---
+++ Miscellaneous/Numerical_Methods/Lagrange.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<math.h>
+#define MAX 10
+
+int main()
+{ 
+    int n;
+    float x[MAX], f[MAX], sum = 0, pro, xp, fp;
+    
+    printf("Enter the number of data points: ");
+    scanf("%d", &n);
+    
+    if (n > MAX || n < 1) {
+        printf("Invalid number of data points [1,%d].", MAX);
+        return 1;
+    }
+    
+    for (int i = 0; i < n; i++) {  
+        printf("\nInput the data points for x[%d] and f[%d]: ", i, i);
+        scanf("%f %f", &x[i], &f[i]);
+    }
+    
+    printf("\nInput the specified value of x: ");
+    scanf("%f", &xp);
+    
+    for (int i = 0; i < n; i++) {  
+        pro = 1.0;
+        for (int j = 0; j < n; j++) {
+            if (i != j)
+                pro *= (xp - x[j]) / (x[i] - x[j]);
+        }
+        sum += pro * f[i];
+    }
+    
+    fp = sum;
+    printf("\nThe required functional value at %f = %f\n", xp, fp);
+    
+    return 0;
+}
